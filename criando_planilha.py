2c2d890a4178968cd35bafd44e8c00a925bed1f5
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-27 13:24:28 -0300

    Add script to create an Excel spreadsheet with fictional book data

---
+++ criando_planilha.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# 1 - Dados ficticios (Livros)
+
+dados = {
+    "Titulo": ["It a coisa", "Outsider", "Misery", "O Instituto", "Bazar dos sonhos ruins"],
+    "Autor": ["Stephen King", "Stephen King", "Stephen King", "Stephen King", "Stephen King"],
+    "Genero": ["Terror", "Terror", "Terror", "Terror", "Terror"],
+    "Ano": [1986, 2018, 1987, 2019, 2015],
+    "Preço": [120.00, 80.00, 75.00, 90.00, 65.00]
+}
+
+# 2 - Criando um dataframe
+
+df = pd.DataFrame(dados)
+
+print(df)
+# OU
+# nome_livro = "livros.xlsx"
+
+df.to_excel("data/livros.xlsx", index=False)
+print("Planilha criada com sucesso!")
